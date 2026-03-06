2a00ab45306afeefe777af665f6b44d6519e8fcd
Noemie <majerusnoemie@gmail.com>
2026-03-06 12:55:20 +0100

    launch internal APIs in startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "Starting ActuLLM..."
+
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+
+uvicorn api_front:app --host 0.0.0.0 --port 8002 &
+uvicorn C3:app --host 0.0.0.0 --port 8004 &
+uvicorn Api_LLM:app --host 0.0.0.0 --port 8005 &
+
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
